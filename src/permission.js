bd2451133c8fd3826a4e23a7f60f6ef041e76037
zhuxiaoxin <1309879645@qq.com>
2022-05-05 10:56:37 +0800

    init

--- src/permission.js
+++ src/permission.js
@@ -1,7 +1,6 @@
 import router from './router'
 
 router.beforeEach((to, from, next) => {
-  console.log(to, 'ssssssss')
   document.title = to.meta.title
   next()
 })
--- src/server/index.js
+++ src/server/index.js
@@ -2,21 +2,27 @@ import axios from 'axios'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  timeout: 5000
+  timeout: 5000 // request timeout
 })
 
+// request interceptor
 service.interceptors.request.use(
   config => {
+    // do something before request is sent
+    // config.headers['X-Token'] = 'xxxxxxxxx'
     return config
   },
   error => {
+    // do something with request error
     return Promise.reject(error)
   }
 )
 
+// response interceptor
 service.interceptors.response.use(
   response => {
     const res = response.data
+    // if the custom code is not 200, it is judged as an error.
     if (res.code !== 200) {
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
