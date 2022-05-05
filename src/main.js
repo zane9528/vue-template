b2d92100770e53410470f1a9356aad047e2f3c1b
zhuxiaoxin <1309879645@qq.com>
2022-05-05 12:06:46 +0000

    update src/main.js.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.scss' // global css
+import './icons' // icon
 import './permission'
 
 Vue.config.productionTip = false
